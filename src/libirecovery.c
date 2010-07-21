756bb5bd823d9b80521e11cf8d7a1ad62604cbce
Martin Szulecki <opensuse@sukimashita.com>
2010-07-21 22:53:55 +0200

    Add check for correct transfer length of commands on sending

--- src/libirecovery.c
+++ src/libirecovery.c
@@ -258,7 +258,7 @@ static irecv_error_t irecv_send_command_raw(irecv_client_t client, unsigned char
 
 	if (length > 0) {
 		int ret = libusb_control_transfer(client->handle, 0x40, 0, 0, 0, command, length + 1, 100);
-		if (ret < 0) {
+		if ((ret < 0) || (ret != (length + 1))) {
 			if (ret == LIBUSB_ERROR_PIPE)
 				return IRECV_E_PIPE;
 
