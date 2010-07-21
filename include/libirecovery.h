59b529e07aff1dbb1cb4673eb2d82e6c8bc94f72
Martin Szulecki <opensuse@sukimashita.com>
2010-07-22 00:26:10 +0200

    Rearrange header function orders into groups

--- include/libirecovery.h
+++ include/libirecovery.h
@@ -93,19 +93,22 @@ irecv_error_t irecv_reset(irecv_client_t client);
 irecv_error_t irecv_close(irecv_client_t client);
 irecv_error_t irecv_receive(irecv_client_t client);
 irecv_error_t irecv_send_exploit(irecv_client_t client);
-irecv_error_t irecv_get_cpid(irecv_client_t client, unsigned int* cpid);
-irecv_error_t irecv_get_bdid(irecv_client_t client, unsigned int* bdid);
-irecv_error_t irecv_send_file(irecv_client_t client, const char* filename);
-irecv_error_t irecv_get_ecid(irecv_client_t client, unsigned long long* ecid);
 irecv_error_t irecv_execute_script(irecv_client_t client, const char* filename);
-irecv_error_t irecv_send_command(irecv_client_t client, unsigned char* command);
 irecv_error_t irecv_set_configuration(irecv_client_t client, int configuration);
+
+irecv_error_t irecv_event_subscribe(irecv_client_t client, irecv_event_type type, irecv_event_cb_t callback, void *user_data);
 irecv_error_t irecv_event_unsubscribe(irecv_client_t client, irecv_event_type type);
+
+irecv_error_t irecv_send_file(irecv_client_t client, const char* filename);
+irecv_error_t irecv_send_command(irecv_client_t client, unsigned char* command);
+irecv_error_t irecv_send_buffer(irecv_client_t client, unsigned char* buffer, unsigned int length);
+
 irecv_error_t irecv_getenv(irecv_client_t client, const char* variable, char** value);
 irecv_error_t irecv_setenv(irecv_client_t client, const char* variable, const char* value);
 irecv_error_t irecv_set_interface(irecv_client_t client, int interface, int alt_interface);
-irecv_error_t irecv_send_buffer(irecv_client_t client, unsigned char* buffer, unsigned int length);
-irecv_error_t irecv_event_subscribe(irecv_client_t client, irecv_event_type type, irecv_event_cb_t callback, void *user_data);
+irecv_error_t irecv_get_cpid(irecv_client_t client, unsigned int* cpid);
+irecv_error_t irecv_get_bdid(irecv_client_t client, unsigned int* bdid);
+irecv_error_t irecv_get_ecid(irecv_client_t client, unsigned long long* ecid);
 
 #ifdef __cplusplus
 }
